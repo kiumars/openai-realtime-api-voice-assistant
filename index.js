a9d00cb1506d42e8d65f35c622af263fe0c32e33
Kayomars Abedian <kiumars_a@yahoo.com>
2026-05-08 13:01:57 -0400

    Update Twilio greeting for clinic

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ fastify.all('/incoming-call', async (request, reply) => {
 
     const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
-                              <Say>Hi, you have called Bart's Automative Centre. How can we help?</Say>
+                              <Say>Hi, you have called Maple Grove Family Clinic. How can we help?</Say>
                               <Connect>
                                   <Stream url="wss://${request.headers.host}/media-stream" />
                               </Connect>
